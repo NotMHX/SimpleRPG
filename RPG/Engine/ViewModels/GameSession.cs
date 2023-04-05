8133657ae2319c4782a50ce3586085d01727e1c4
LucaDalCorso2004 <L.DalCorso.inf21@stud.bbbaden.ch>
2023-04-05 09:07:30 +0200

    create class GameSeassion

---
+++ RPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player Currentplay { get; set; }
+        public GameSession() {
+        
+        Currentplay= new Player();
+
+            Currentplay.Name = "Micheal";
+            Currentplay.Level = 1;
+            Currentplay.HitPoints = 5;
+            Currentplay.ExperiencePoints = 0;
+            Currentplay.CharacterClass = "Swordsman";
+            Currentplay.Gold = 2;
+        }
+
+    }
+}
