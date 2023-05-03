a5b1a154057578c116a8733f21cdad676bba5391
notmhx <michaelherrmann111104@gmail.com>
2023-05-03 11:38:29 +0200

    delete unused code

--- RPG/Engine/Factory/Worldfactory.cs
+++ RPG/Engine/Factory/Worldfactory.cs
@@ -1,12 +1,14 @@
-﻿using System.IO;
-using System.Xml;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Engine.Models;
 namespace Engine.Factory
 {
-    internal static class Worldfactory
+    internal class Worldfactory
     {
-        private const string GAME_DATA_FILENAME = ".\\GameData\\Locations.xml";
-        internal static World CreateWorld()
+        internal World Createworld()
         {
             World world = new World();
             if (File.Exists(GAME_DATA_FILENAME))
@@ -25,59 +27,6 @@ namespace Engine.Factory
                 throw new FileNotFoundException($"Missing data file: {GAME_DATA_FILENAME}");
             }
             return world;
-        }
-        private static void LoadLocationsFromNodes(World world, string rootImagePath, XmlNodeList nodes)
-        {
-            if (nodes == null)
-            {
-                return;
-            }
-            foreach (XmlNode node in nodes)
-            {
-                Location location =
-                    new Location(node.AttributeAsInt("X"),
-                                 node.AttributeAsInt("Y"),
-                                 node.AttributeAsString("Name"),
-                                 node.SelectSingleNode("./Description")?.InnerText ?? "",
-                                 $".{rootImagePath}{node.AttributeAsString("ImageName")}");
-                AddMonsters(location, node.SelectNodes("./Monsters/Monster"));
-                AddQuests(location, node.SelectNodes("./Quests/Quest"));
-                AddTrader(location, node.SelectSingleNode("./Trader"));
-                world.AddLocation(location);
-            }
-        }
-        private static void AddMonsters(Location location, XmlNodeList monsters)
-        {
-            if (monsters == null)
-            {
-                return;
-            }
-            foreach (XmlNode monsterNode in monsters)
-            {
-                location.AddMonster(monsterNode.AttributeAsInt("ID"),
-                                    monsterNode.AttributeAsInt("Percent"));
-            }
-        }
-        private static void AddQuests(Location location, XmlNodeList quests)
-        {
-            if (quests == null)
-            {
-                return;
-            }
-            foreach (XmlNode questNode in quests)
-            {
-                location.QuestsAvailableHere
-                        .Add(QuestFactory.GetQuestByID(questNode.AttributeAsInt("ID")));
-            }
-        }
-        private static void AddTrader(Location location, XmlNode traderHere)
-        {
-            if (traderHere == null)
-            {
-                return;
-            }
-            location.TraderHere =
-                TraderFactory.GetTraderByName(traderHere.AttributeAsString("Name"));
         }
     }
 }
