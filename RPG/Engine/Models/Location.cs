1e22c1a17c2f1c0c169dd5b8c8df32be87c06a67
notmhx <michaelherrmann111104@gmail.com>
2023-05-10 09:11:27 +0200

    added monster encounter class and modified location class

--- RPG/Engine/Models/Location.cs
+++ RPG/Engine/Models/Location.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,47 @@ namespace Engine.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageName { get; set; }
+
+        public List<MonsterEncounter> MonstersHere { get; set; } = new List<MonsterEncounter>();
+
+        public void AddMonster(int monsterID, int chanceOfEncountering)
+        {
+            if (MonstersHere.Exists(m => m.MonsterID == monsterID)) // If there is a monster already
+            {
+                MonstersHere.First(m => m.MonsterID == monsterID).ChanceOfEncountering = chanceOfEncountering; // Add chance to existing monster
+            }
+            else
+            {
+                MonstersHere.Add(new MonsterEncounter(monsterID, chanceOfEncountering)); // Adds new monster if there's none yet
+            }
+        }
+
+        public Monster GetMonster()
+        {
+            if (!MonstersHere.Any())
+            {
+                return null; // If there's no monster
+            }
+
+            int totalChances = MonstersHere.Sum(m => m.ChanceOfEncountering); // Total chance of encountering
+
+            int randomNumber = RandomNumberGenerator.NumberBetween(1, totalChances);
+
+            int total = 0;
+
+            foreach (MonsterEncounter monsterEncounter in MonstersHere)
+            {
+                total += monsterEncounter.ChanceOfEncountering; // adds current chance to total
+
+                if (randomNumber <= total) // if total is less than the max number of monsters
+                {
+                    return MonsterFactory.GetMonster(monsterEncounter.MonsterID);
+                }
+            }
+
+            // if there's none
+            return MonsterFactory.GetMonster(MonstersHere.Last().MonsterID);
+
+        }
     }
 }
